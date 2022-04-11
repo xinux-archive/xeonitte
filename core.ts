03e968316bc817405d81293adf7f0ad4c3f6a540
UwUssimo Robinson <uwussimo@icloud.com>
2022-04-11 07:41:34 +0500

    Update core.ts

--- core.ts
+++ core.ts
@@ -7,13 +7,13 @@ export const bot = new Bot(env["TOKEN"] || "");
 export const handle = webhookCallback(bot, "std/http");
 
 const initializer = async () => {
-  bot.on("message:text", async (ctx: Context) => {
-    if (ctx?.message?.from?.username) {
-      if (ctx?.message?.from?.username === "Channel_Bot") {
-        await ctx.deleteMessage();
-      }
-    }
-  });
+  // bot.on("message:text", async (ctx: Context) => {
+  //   if (ctx?.message?.from?.username) {
+  //     if (ctx?.message?.from?.username === "Channel_Bot") {
+  //       await ctx.deleteMessage();
+  //     }
+  //   }
+  // });
   await console.log(blue("[INFO]"), `bot is starting on ${env["HOST"]}`);
   await delta(bot);
   await bot.catch((error) => {
