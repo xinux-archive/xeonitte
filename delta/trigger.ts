827c2b02bfd5c826bb007b535e587e10dc6fb9cf
UwUssimo Robinson <uwussimo@icloud.com>
2022-05-17 14:28:52 +0500

    Update trigger.ts

--- delta/trigger.ts
+++ delta/trigger.ts
@@ -40,6 +40,43 @@ composer.command("off", isReply, async (ctx: Context): Promise<void> => {
   }
 });
 
+composer.command("nonoff", isReply, async (ctx: Context): Promise<void> => {
+  if (ctx?.message?.reply_to_message?.from?.id === ctx.me.id) {
+    await ctx.reply(`Ha-ha... yaxshi urinish!`, {
+      parse_mode: "HTML",
+    });
+  } else {
+    await ctx.api.deleteMessage(
+        ctx.message!.chat!.id,
+        ctx.message!.reply_to_message!.message_id,
+    );
+    await ctx.api.deleteMessage(
+        ctx.message!.chat!.id,
+        ctx.message!.message_id,
+    );
+    await ctx.reply(
+        `<b>Hurmatli <a href="tg://user?id=${ctx?.message?.reply_to_message
+            ?.from?.id}">${ctx?.message?.reply_to_message?.from
+            ?.first_name}</a>,</b>` +
+        `\n` +
+        `\n` +
+        `Chunishim bo'yicha siz mavzuga kirib ketayabsiz. Iltimos, ` +
+        `quyidagi tugmachani bosish orqali bizning asosiy guruhga o'tib oling! ` +
+        `Linux haqida iloji boricha asosiy guruhimizda suhbatlashish tavsiya etiladi. Offtopchilarga halaqit qilmayliga 😉` +
+        `\n` +
+        `\n` +
+        `<b>Hurmat ila, Xeonitte (Kseyonita)</b>`,
+        {
+          parse_mode: "HTML",
+          reply_markup: new InlineKeyboard().url(
+              `Asosiy Chat`,
+              `https://t.me/xinuxuz`,
+          ),
+        },
+    );
+  }
+});
+
 composer.command("doc", isReply, async (ctx: Context): Promise<void> => {
   if (ctx?.message?.reply_to_message?.from?.id === ctx.me.id) {
     await ctx.reply(`Ha-ha... yaxshi urinish!`, {
