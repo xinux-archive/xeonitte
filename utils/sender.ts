2ceadd6b397f63300c7d2f5ab14ef65fbbf3ae90
UwUssimo Robinson <uwussimo@icloud.com>
2022-11-14 17:08:22 +0500

    [RM] removing unused sendMessage custom shortcut

--- utils/sender.ts
+++ utils/sender.ts
@@ -26,24 +26,3 @@ export const reply = async (
 
   return await ctx.reply(message, config);
 };
-
-export const sendMessage = async (
-  ctx: Context,
-  where: number | string,
-  message: string,
-  buttons?: InlineKeyboard,
-): Promise<any> => {
-  const config: { [key: string]: any } = {
-    parse_mode: "HTML",
-  };
-
-  if (ctx.message!.is_topic_message) {
-    config["message_thread_id"] = ctx.message!.message_thread_id;
-  }
-
-  if (buttons) {
-    config["reply_markup"] = buttons;
-  }
-
-  return await ctx.api.sendMessage(where, message, config);
-};
