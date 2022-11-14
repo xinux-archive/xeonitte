fc0cdf26f7bc59da439e94511ee4bfe8b7d34888
UwUssimo Robinson <uwussimo@icloud.com>
2022-11-14 14:41:25 +0500

    Update isReply.ts

--- hooks/isReply.ts
+++ hooks/isReply.ts
@@ -2,7 +2,7 @@ import { Context, NextFunction } from "../deps.ts";
 import { reply } from "../utils/sender.ts";
 
 export default async (ctx: Context, next: NextFunction) => {
-  console.log(ctx.message)
+  console.log(ctx.message);
   if (!ctx.message?.reply_to_message) {
     return await reply(ctx, `↪ Reply bilan ko'rsatingchi habarni!`);
   }
