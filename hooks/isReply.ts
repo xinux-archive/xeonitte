9f0376e7778a57471f990277eeaa3145f5d88dab
UwUssimo Robinson <uwussimo@icloud.com>
2022-11-14 16:18:48 +0500

    Update isReply.ts

--- hooks/isReply.ts
+++ hooks/isReply.ts
@@ -12,7 +12,7 @@ export default async (ctx: Context, next: NextFunction) => {
 
   console.log("Message:", ctx.message.message_id);
   console.log("Reply:", ctx.message.reply_to_message?.message_id);
-  console.log("Contain:", !Object.values(topics).includes(ctx.message.reply_to_message?.message_id))
+  console.log("Contain:", !Object.values(topics).includes(ctx.message!.reply_to_message!.message_id))
   console.log("Reply:", !ctx.message?.reply_to_message)
 
   if (!ctx.message?.reply_to_message && !Object.values(topics).includes(ctx.message!.message_id)) {
