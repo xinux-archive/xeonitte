9d786dfa3e295e0258740b225456fab52ddd3080
akumarujon <isakulovdev@gmail.com>
2023-09-21 19:04:54 +0500

    added more topics & removed if statement.

--- hooks/isReply.ts
+++ hooks/isReply.ts
@@ -3,12 +3,6 @@ import { Context, NextFunction } from "../deps.ts";
 import topics from "../topics.json" assert { type: "json" };
 
 export default async (ctx: Context, next: NextFunction) => {
-  if (!ctx.message?.is_topic_message) {
-    return await reply(
-      ctx,
-      `⚠️ <b>Biz topicda emasmiz bu komandani ishlatish uchun!</b>`,
-    );
-  }
 
   if (
     !ctx.message?.reply_to_message ||
@@ -17,4 +11,5 @@ export default async (ctx: Context, next: NextFunction) => {
     return await reply(ctx, `↪ Reply bilan ko'rsatingchi habarni!`);
   }
   await next();
+  
 };
